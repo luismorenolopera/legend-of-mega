f9a1ba73a02f5b49b7f5840f51868564dedfec05
David Moreno <david.moreno@edunext.co>
2020-05-04 11:08:59 -0500

    Add anime card component

---
+++ components/AnimeCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-card
+    class='mx-auto'
+    max-width='400'
+  >
+    <v-img
+      class='white--text align-end'
+      max-height="400"
+      :src='image'
+    >
+      <v-card-title>{{name}}</v-card-title>
+      <v-card-subtitle>{{type}} {{number}}</v-card-subtitle>
+    </v-img>
+
+    
+  </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        id: Number,
+        name: String,
+        number: Number,
+        type: String,
+        image: String
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -9,84 +9,70 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for='anime in animes'
+          :key='anime.id'
+          lg='3'
+          md='6'
+          xs='12'
+        >
+          <anime-card
+            class='ma-3'
+            :id='anime.id'
+            :name='anime.name'
+            :number='anime.number'
+            :type='anime.type'
+            :image='anime.image'
+            :link='anime.link'
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+
+          </anime-card>
+        </v-col>
+
+      </v-row>
+    </v-container>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+import AnimeCard from '~/components/AnimeCard.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    AnimeCard
+  },
+  data () {
+    return {
+      animes: [
+        {
+          id: 1,
+          name: 'naruto',
+          number: 1,
+          type: 'episodio',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+          link: 'naruto'
+        },
+        {
+          id:2,
+          name: 'afro samurai',
+          number: null,
+          type: 'pelicula',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+          link: 'afro-samurai'
+        },
+        {
+          id:3,
+          name: 'afro samurai',
+          number: null,
+          type: 'pelicula',
+          image: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'
+        }
+      ]
+    }
   }
 }
 </script>
